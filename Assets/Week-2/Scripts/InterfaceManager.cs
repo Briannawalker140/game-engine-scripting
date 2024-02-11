47ddf08c2f2d2b9e77e578090c3c4cba672f5f17
BRIANNA\cheir <Briannawalker6@icloud.com>
2024-02-10 18:04:04 -0600

    Week-2 Initial Project Files Commit

---
+++ Assets/Week-2/Scripts/InterfaceManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InterfaceManager : MonoBehaviour
+{
+    public TextMeshProUGUI label;
+    public void PrintMessage(string msg)
+       {
+        label.text = msg;
+        
+       }
+
+}
