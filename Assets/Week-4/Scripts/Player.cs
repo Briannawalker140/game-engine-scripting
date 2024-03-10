d1282d25c9465870bdb7baef603db852e8d2daf4
BRIANNA\cheir <Briannawalker6@icloud.com>
2024-03-09 18:30:05 -0600

    Week-4 Bee Game Commit

---
+++ Assets/Week-4/Scripts/Player.cs
@@ -0,0 +1,49 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BeeGame
+{
+
+    public class Player : MonoBehaviour
+    {
+        public int health = 10;
+
+        public void Damage(int amt)
+        {
+            health -= amt;
+        }
+
+        private Enemy FindNewTarget()
+        {
+            /*GameObject[] enemyObjects = GameObject.FindGameObjectsWithTag("EnemyTag");
+            int randomIndex = Random.Range(0, enemyObjects.Length);
+            return enemyObjects[randomIndex].GetComponent<Enemy>();*/
+
+            /*GameObject enemyObject = GameObject.Find("EnemyObject");
+            return enemyObject.GetComponent<Enemy>();*/
+
+            Enemy[] enemies = FindObjectsByType<Enemy>(FindObjectsSortMode.None);
+            int randomIndex = Random.Range(0, enemies.Length);
+            return enemies[randomIndex];
+        }
+
+        [ContextMenu("Attack")]
+        void Attack()
+        {
+            Enemy target = FindNewTarget();
+
+            Vector3 origin = transform.position;
+
+            transform.DOMove(target.transform.position, 1f)
+                      .OnComplete(() =>
+                      {
+                          target.Damage(10);
+
+                          transform.DOMove(target.transform.position, 1f);
+                      });
+                      
+        }
+    }
+}
